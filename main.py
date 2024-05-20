b346e46d8bbdc958b15e7757d70d7e0aa583a3b4
InsertNameHereK1A <131831124+InsertNameHereK1A@users.noreply.github.com>
2024-05-20 02:44:21 +0000

    lots of work done did mopre json refining

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 # loading the json file
 def LoadJsonData(filename):
   if not os.path.exists(filename):
@@ -10,15 +11,11 @@ def LoadJsonData(filename):
   with open(filename, 'r') as file:
     return json.load(file)
 
+
 #saving the json file
 def SaveJsonData(filename, data):
   with open(filename, 'w') as file:
     json.dump(data, file)
-
-
-
-
-
 
 
 # funtion that returns everything about the ingredient
@@ -27,6 +24,7 @@ def ingredientInfo():
   amount = input("How much of the ingredient do you want to add? ")
   cost = input("How much does it cost? $")
   return [type, amount, cost]
+
 
 # funtion that ask if you would like to add mopre ingredients
 def MoreIngredients():
@@ -38,10 +36,10 @@ def MoreIngredients():
       return True
     print('invalid input')
 
+
 # loading the json file
 JsonFilename = 'recipe.json'
 JsonData = LoadJsonData(JsonFilename)
-
 
 #WELCOME THE USER
 print('''
@@ -69,20 +67,20 @@ if RecipeTitle in JsonData:
       exit()
     else:
       print("invalid input")
-    
+
 ServingSize = int(input("How many people does this recipe serve? "))
 #Getting ingredients
 GettingIngredients = True
 while GettingIngredients is True:
-#Asking if they want to add more ingredients
+  #Asking if they want to add more ingredients
   Recipe.append(ingredientInfo())
   GettingIngredients = MoreIngredients()
-  
+
 #Saving the recipe
 
-SaveJsonData('recipe.json',Recipe)
+SaveJsonData('recipe.json', Recipe)
 #getting total cost per serving
 TotalCostPerServing = 0
-for i in range(0,len(Recipe)):
+for i in range(0, len(Recipe)):
   TotalCostPerServing = TotalCostPerServing + int(Recipe[i][2])
 print(f"{RecipeTitle}'s cost per serving is {TotalCostPerServing}")
